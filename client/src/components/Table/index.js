b02c986774d06a6153c87db94ca436eff1ddfb60
Tommy Flynn <TFly.Flynn@gmail.com>
2019-12-23 18:04:23 -0500

    added more rows to the table with updated NBA scores

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -13,6 +13,12 @@ class Table extends Component {
         allNBA: [],
         tomNBA: "",
         patrickNBA: "",
+        jamesNBA: "",
+        neptuneNBA: "",
+        DJNBA:"",
+        gooseNBA:"",
+        alNBA:"",
+        joeNBA:"",
     }
 
     componentDidMount() {
@@ -47,8 +53,6 @@ class Table extends Component {
                 console.log(TomPoints);
                 this.setState({ tomNBA: TomPoints });
 
-                // NOW I NEED TO RECREATE THIS FOR ALL 10 TEAMS IN THE EFL LEAGUE. 
-
                 // PATRICK
                 var lakersWin = res.data.api.standings[23].win;
                 var warriorsWin = res.data.api.standings[20].win;
@@ -68,7 +72,133 @@ class Table extends Component {
                     PatrickPoints += patDoubledScores[i];
                 }
                 console.log(PatrickPoints);
-                this.setState({ patrickNBA: PatrickPoints })
+                this.setState({ patrickNBA: PatrickPoints });
+
+                // James
+                var bucksWin = res.data.api.standings[10].win;
+                var thunderWin = res.data.api.standings[28].win;
+                var sunsWin = res.data.api.standings[24].win;
+
+                const tempJamesNBA = [];
+
+                tempJamesNBA.push(bucksWin);
+                tempJamesNBA.push(thunderWin);
+                tempJamesNBA.push(sunsWin);
+
+                var jamesDoubledScores = tempJamesNBA.map(team => team * 2);
+
+                var jamesPoints = 0;
+
+                for (var i = 0; i < jamesDoubledScores.length; i++) {
+                    jamesPoints += jamesDoubledScores[i];
+                }
+                console.log(jamesPoints);
+                this.setState({ jamesNBA: jamesPoints });
+
+                // Neptune
+                var celticsWin = res.data.api.standings[7].win;
+                var pacersWin = res.data.api.standings[11].win;
+                var hornetsWin = res.data.api.standings[1].win;
+
+                const tempNeptuneNBA = [];
+
+                tempNeptuneNBA.push(celticsWin);
+                tempNeptuneNBA.push(pacersWin);
+                tempNeptuneNBA.push(hornetsWin);
+
+                var NeptuneDoubledScores = tempNeptuneNBA.map(team => team * 2);
+
+                var NeptunePoints = 0;
+
+                for (var i = 0; i < NeptuneDoubledScores.length; i++) {
+                    NeptunePoints += NeptuneDoubledScores[i];
+                }
+                console.log(NeptunePoints);
+                this.setState({ neptuneNBA: NeptunePoints });
+
+                // DJ
+                var sixersWin = res.data.api.standings[6].win;
+                var rocketsWin = res.data.api.standings[15].win;
+                var timberwolvesWin = res.data.api.standings[29].win;
+
+                const tempDJNBA = [];
+
+                tempDJNBA.push(sixersWin);
+                tempDJNBA.push(rocketsWin);
+                tempDJNBA.push(timberwolvesWin);
+
+                var DJDoubledScores = tempDJNBA.map(team => team * 2);
+
+                var DJPoints = 0;
+
+                for (var i = 0; i < DJDoubledScores.length; i++) {
+                    DJPoints += DJDoubledScores[i];
+                }
+                console.log(DJPoints);
+                this.setState({ DJNBA: DJPoints });
+
+                 // Goose
+                 var raptorsWin = res.data.api.standings[5].win;
+                 var pistonsWin = res.data.api.standings[12].win;
+                 var knicksWin = res.data.api.standings[9].win;
+ 
+                 const tempGooseNBA = [];
+ 
+                 tempGooseNBA.push(raptorsWin);
+                 tempGooseNBA.push(pistonsWin);
+                 tempGooseNBA.push(knicksWin);
+ 
+                 var GooseDoubledScores = tempGooseNBA.map(team => team * 2);
+ 
+                 var GoosePoints = 0;
+ 
+                 for (var i = 0; i < GooseDoubledScores.length; i++) {
+                     GoosePoints += GooseDoubledScores[i];
+                 }
+                 console.log(GoosePoints);
+                 this.setState({ gooseNBA: GoosePoints });
+
+                  // Al
+                  var mavericksWin = res.data.api.standings[19].win;
+                  var magicWin = res.data.api.standings[0].win;
+                  var wizardsWin = res.data.api.standings[3].win;
+  
+                  const tempAlNBA = [];
+  
+                  tempAlNBA.push(mavericksWin);
+                  tempAlNBA.push(magicWin);
+                  tempAlNBA.push(wizardsWin);
+  
+                  var AlDoubledScores = tempAlNBA.map(team => team * 2);
+  
+                  var AlPoints = 0;
+  
+                  for (var i = 0; i < AlDoubledScores.length; i++) {
+                      AlPoints += AlDoubledScores[i];
+                  }
+                  console.log(AlPoints);
+                  this.setState({ alNBA: AlPoints });
+
+                  // Joe
+                  var nuggetsWin = res.data.api.standings[25].win;
+                  var blazersWin = res.data.api.standings[26].win;
+                  var kingsWin = res.data.api.standings[22].win;
+  
+                  const tempJoeNBA = [];
+  
+                  tempJoeNBA.push(nuggetsWin);
+                  tempJoeNBA.push(blazersWin);
+                  tempJoeNBA.push(kingsWin);
+  
+                  var JoeDoubledScores = tempJoeNBA.map(team => team * 2);
+  
+                  var JoePoints = 0;
+  
+                  for (var i = 0; i < JoeDoubledScores.length; i++) {
+                      JoePoints += JoeDoubledScores[i];
+                  }
+                  console.log(JoePoints);
+                  this.setState({ joeNBA: JoePoints });
             })
             .catch(error => {
                 console.log(error)
@@ -119,8 +249,6 @@ class Table extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* returning the state. Eventually this will be an API call to the mongo database, which will include each user's team info. For now, going to have a hard-coded table. */}
-                        {/* {this.returnTable()} */}
                         <tr>
                             <th scope="col">Tommy</th>
                             <th scope="col">EPL</th>
@@ -130,11 +258,8 @@ class Table extends Component {
                             <th scope="col">MLB</th>
                             <th scope="col">Total Points</th>
                         </tr>
-
                     </tbody>
                     <tbody>
-                        {/* returning the state. Eventually this will be an API call to the mongo database, which will include each user's team info. For now, going to have a hard-coded table. */}
-                        {/* {this.returnTable()} */}
                         <tr>
                             <th scope="col">Patrick</th>
                             <th scope="col">EPL</th>
@@ -144,7 +269,72 @@ class Table extends Component {
                             <th scope="col">MLB</th>
                             <th scope="col">Total Points</th>
                         </tr>
-
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <th scope="col">James</th>
+                            <th scope="col">EPL</th>
+                            <th scope="col">NFL</th>
+                            <th scope="col">NHL</th>
+                            <th scope="col">{this.state.jamesNBA}</th>
+                            <th scope="col">MLB</th>
+                            <th scope="col">Total Points</th>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <th scope="col">Neptune</th>
+                            <th scope="col">EPL</th>
+                            <th scope="col">NFL</th>
+                            <th scope="col">NHL</th>
+                            <th scope="col">{this.state.neptuneNBA}</th>
+                            <th scope="col">MLB</th>
+                            <th scope="col">Total Points</th>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <th scope="col">DJ</th>
+                            <th scope="col">EPL</th>
+                            <th scope="col">NFL</th>
+                            <th scope="col">NHL</th>
+                            <th scope="col">{this.state.DJNBA}</th>
+                            <th scope="col">MLB</th>
+                            <th scope="col">Total Points</th>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <th scope="col">Goose</th>
+                            <th scope="col">EPL</th>
+                            <th scope="col">NFL</th>
+                            <th scope="col">NHL</th>
+                            <th scope="col">{this.state.gooseNBA}</th>
+                            <th scope="col">MLB</th>
+                            <th scope="col">Total Points</th>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <th scope="col">Al</th>
+                            <th scope="col">EPL</th>
+                            <th scope="col">NFL</th>
+                            <th scope="col">NHL</th>
+                            <th scope="col">{this.state.alNBA}</th>
+                            <th scope="col">MLB</th>
+                            <th scope="col">Total Points</th>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <th scope="col">Joe</th>
+                            <th scope="col">EPL</th>
+                            <th scope="col">NFL</th>
+                            <th scope="col">NHL</th>
+                            <th scope="col">{this.state.joeNBA}</th>
+                            <th scope="col">MLB</th>
+                            <th scope="col">Total Points</th>
+                        </tr>
                     </tbody>
                 </table>
             </div>
