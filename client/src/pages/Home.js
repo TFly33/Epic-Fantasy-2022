bc8a069ad7c40bbd3fdffbf23cbe734dc6afdf5d
Tommy Flynn <TFly.Flynn@gmail.com>
2020-01-08 13:19:54 -0500

    fixed the footer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -51,29 +51,18 @@ class Home extends React.Component {
                     </div>
                 </nav>
                 <div class="container specialTable">
-                <Table />
+                    <Table />
                 </div>
                 {/* <div class ="container">
                 <DatatablePage/>
                 </div> */}
-                <body class="d-flex flex-column">
-                    <div id="page-content">
-                        <div class="container text-center">
-                            <div class="row justify-content-center">
-                                <div class="col-md-7">
-                                    {/* Adding this custom footer I found online. Requires some fake text. Here it is below.  */}
-                                    <h1 class="font-weight-light mt-4 text-white">Sticky Footer using Flexbox</h1>
-                                    <p class="lead text-white-50">Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!</p>
-                                </div>
-                            </div>
-                        </div>
+
+                <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+                    <div class="container text-center">
+                        <small>Copyright &copy; Epic Fantasy League 2020</small>
                     </div>
-                    <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
-                        <div class="container text-center">
-                            <small>Copyright &copy; Epic Fantasy League 2020</small>
-                        </div>
-                    </footer>
-                </body>
+                </footer>
+
             </div>
         )
     }
